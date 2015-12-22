59c17df6583b20add4faed4d8a411cdb494189cf
Jordan <jminjie@umich.edu>
2015-12-22 17:19:25 -0500

    remove unneeded function and fix package use

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.stats as st
-import statistics as stats
 
 # Generates random data points from N(0, 1)
 # e.g.
@@ -19,22 +18,12 @@ def generate_random_data(num_points=100, num_features=30):
         data_points.append(data_point)
     return data_points
 
-# Given a set of points, returns the average point
-def get_average_point(data):
-    num_features = len(data[0])
-    average = [0 for i in range(0, num_features)]
-    for point in data:
-        for feature in range(0, num_features):
-            average[feature] += point[feature]
-    for feature in range(0, num_features):
-        average[feature] /= len(data)
-
 # Given a list of N data points with the same number of features
 # returns the indexes of the features found in Treatment but not Placebo
 # with p < 0.05
 def find_significant_features(control_data, treatment_data, pval=0.05):
-    num_points = len(data)
-    num_features = len(data[0])
+    num_points = len(treatment_data)
+    num_features = len(treatment_data[0])
     significant_features = []
     threshold = st.norm.ppf(1 - pval)
     if num_points is 0 or num_features is 0:
@@ -50,7 +39,7 @@ def find_significant_features(control_data, treatment_data, pval=0.05):
                        instead of {2}'.format(point, len(point), num_features)
                 return
             current_feature_control.append(point[current_feature])
-        population_mean = stats.mean(current_feature_control)
+        population_mean = np.mean(current_feature_control)
         # Get t statistic of treatment data
         current_feature_treatment = []
         for point in treatment_data:
@@ -59,16 +48,15 @@ def find_significant_features(control_data, treatment_data, pval=0.05):
                        instead of {2}'.format(point, len(point), num_features)
                 return
             current_feature_treatment.append(point[current_feature])
-        treatment_mean = stats.mean(current_feature_treatment)
-        stdev = stats.stdev(current_feature_treatment)
+        treatment_mean = np.mean(current_feature_treatment)
+        stdev = np.std(current_feature_treatment)
         t_statistic = (treatment_mean-population_mean) / (stdev/ \
                                                           math.sqrt(num_points))
         # Check if treatment data is significant on this feature
         if t_statistic > threshold:
             significant_features.append(current_feature)
+    return significant_features
         
-        
-
 def main():
     treatment_data = generate_random_data()
     control_data = generate_random_data()
